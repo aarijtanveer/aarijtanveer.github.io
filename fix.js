51bdd577a8a49cbbbfdfc6886e64c121e61e3b1b
Muhammad Arij Tanveer <aarij.tanveer1@gmail.com>
2026-03-25 09:16:00 +0500

    Delete fix.js

--- fix.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-let content = fs.readFileSync('index.html', 'utf8');
-content = content.replace(/\\`/g, '`');
-content = content.replace(/\\\${/g, '${');
-content = content.replace(/\\\\d/g, '\\d');
-content = content.replace(/\\\\s/g, '\\s');
-fs.writeFileSync('index.html', content);
-console.log('Fixed index.html');
