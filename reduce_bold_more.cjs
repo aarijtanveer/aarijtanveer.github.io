344dca4d07e7a07e24323d7f3f2dfae7e69fa219
Muhammad Arij Tanveer <aarij.tanveer1@gmail.com>
2026-03-25 09:22:38 +0500

    Delete reduce_bold_more.cjs

--- reduce_bold_more.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('index.html', 'utf8');
-html = html.replace(/font-bold/g, 'font-semibold');
-fs.writeFileSync('index.html', html);
-console.log('Boldness reduced further.');
